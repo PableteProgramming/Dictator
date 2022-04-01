4e0cc782550a3a188d812b3b0211ea9b89cbf29f
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2022-04-01 20:53:33 +0200

    Globally working but some bugs to fix

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Dictator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pausebtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ReadBtn
+            // 
+            this.ReadBtn.Location = new System.Drawing.Point(12, 373);
+            this.ReadBtn.Name = "ReadBtn";
+            this.ReadBtn.Size = new System.Drawing.Size(263, 23);
+            this.ReadBtn.TabIndex = 0;
+            this.ReadBtn.Text = "Lire";
+            this.ReadBtn.UseVisualStyleBackColor = true;
+            this.ReadBtn.Click += new System.EventHandler(this.ReadBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 323);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(306, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lecteur de texte";
+            // 
+            // Pausebtn
+            // 
+            this.Pausebtn.Location = new System.Drawing.Point(281, 373);
+            this.Pausebtn.Name = "Pausebtn";
+            this.Pausebtn.Size = new System.Drawing.Size(261, 23);
+            this.Pausebtn.TabIndex = 3;
+            this.Pausebtn.Text = "Pause";
+            this.Pausebtn.UseVisualStyleBackColor = true;
+            this.Pausebtn.Click += new System.EventHandler(this.Pausebtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(548, 373);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(240, 23);
+            this.StopBtn.TabIndex = 4;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.Pausebtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ReadBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Pausebtn;
+        private System.Windows.Forms.Button StopBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Globalization;
+using System.Threading;
+
+namespace Dictator
+{
+    public partial class Form1 : Form
+    {
+        public bool reading;
+        public SpeechSynthesizer synthetiser;
+        public bool textchanged;
+        public Thread CheckState;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Pausebtn.Enabled = false;
+            StopBtn.Enabled = false;
+            reading = false;
+            textchanged = false;
+        }
+
+        private void CheckReadState()
+        {
+            int oldState = 0;
+            String state = synthetiser.State.ToString();
+            label1.Text = state;
+            while (reading)
+            {
+                if ((int)synthetiser.State != oldState)
+                {
+                    //state changed
+                    int newState = ((int)synthetiser.State);
+                    if (newState == 2)
+                    {
+                        oldState = 2;
+                        //Paused
+                        Pausebtn.Enabled = false;
+                        StopBtn.Enabled = true;
+                        ReadBtn.Enabled = true;
+                    }
+                    else if (newState == 1)
+                    {
+                        oldState = 1;
+                        //Speaks again
+                        Pausebtn.Enabled = true;
+                        ReadBtn.Enabled = false;
+                        StopBtn.Enabled = true;
+                    }
+                }
+            }
+            //if i change text and then click on read again, it finished directly, as if it were not reading (=false)
+            label1.Text = "finished";
+            textchanged = false;
+            ReadBtn.Enabled = true;
+            Pausebtn.Enabled = false;
+            StopBtn.Enabled = false;
+        }
+
+        public void Speak_done(object sender, SpeakCompletedEventArgs e)
+        {
+            reading = false;
+            textchanged = false;
+        }
+
+        public void ReadText(String text)
+        {
+            try
+            {
+                CheckState.Abort();
+                label1.Text = "Process aborted";
+            }
+            catch (NullReferenceException) { }
+            synthetiser = new SpeechSynthesizer();
+            synthetiser.SetOutputToDefaultAudioDevice();
+            synthetiser.SpeakAsync(text);
+            synthetiser.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(Speak_done);
+            reading = true;
+            CheckState = new Thread(new ThreadStart(CheckReadState));
+            Pausebtn.Enabled = true;
+            ReadBtn.Enabled = false;
+            StopBtn.Enabled = true;
+            CheckState.Start();
+        }
+
+        private void ReadBtn_Click(object sender, EventArgs e)
+        {
+            if (!reading)
+            {
+                //not reading yet
+                textchanged = false;
+                String text = textBox1.Text;
+                ReadText(text);
+            }
+            else
+            {
+                //Just paused
+                if (textchanged)
+                {
+                    label1.Text = "textchanged";
+                    textchanged = false;
+                    synthetiser.SpeakAsyncCancelAll();
+                    ReadText(textBox1.Text);
+                }
+                else
+                {
+                    synthetiser.Resume();
+                }
+            }
+        }
+
+        private void Pausebtn_Click(object sender, EventArgs e)
+        {
+            synthetiser.Pause();
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            synthetiser.SpeakAsyncCancelAll();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (reading)
+                {
+                    textchanged = true;
+                }
+            }
+            catch (NullReferenceException) { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try{
+                synthetiser.SpeakAsyncCancelAll();
+            }catch (NullReferenceException) { }
+        }
+    }
+}
